3ab1268681ca805f734039f0a9d44db9f1cc85c1
Daniel Santos <tkdsantos@gmail.com>
2019-08-06 09:26:26 -0300

    Listando agenda de prestador

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderControlller from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,7 @@ routes.get('/providers', ProviderControlller.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
