6f290aa15c96327e46ff77053ae3458d2f4f6770
Daniel Santos <tkdsantos@gmail.com>
2019-08-20 08:00:44 -0300

    chore: Listando horários disponíveis

---
+++ src/app/controllers/AvailableController.js
@@ -0,0 +1,81 @@
+/**
+ * Precisa desses dois atributos pois queremos listar todos os agendamentos desde o inicio do dia (startofDat)
+ * até o fim do dia (endOfDay) que o usuário informou
+ */
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: ' Invalid date' });
+    }
+
+    const searchDate = Number(date);
+
+    /**
+     * Fazendo a listagem de agendamentos do dia que o usuario informou
+     */
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    /**
+     * Todos os horarios disponivel que um provider possui
+     */
+    const schedule = [
+      '08:00', // 2018-06-23 08:00:00
+      '09:00', // 2018-06-23 09:00:00
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    /**
+     * Vai retornar os horarios diponível para o usuario
+     */
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
+  }
+}
+
+export default new AvailableController();
